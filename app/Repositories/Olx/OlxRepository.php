e9116f8740c3bd206f2634ff93ee9987b2a1dfe6
Douglas Espindola <douglas.spindola15@gmail.com>
2023-10-01 23:30:30 -0300

    created method to return the parameters

--- app/Repositories/Olx/OlxRepository.php
+++ app/Repositories/Olx/OlxRepository.php
@@ -8,25 +8,28 @@
 
     class OlxRepository
     {
-        public static function getScrapProperties(): Response
+        public static function getScrapProperties($params): Response
         {
-            $vagasGaragem = 1;
+            $baseUrl = "https://www.olx.com.br/imoveis/aluguel/estado-pa";
+            $queryString = http_build_query($params);
+            $url = $baseUrl . '?' . $queryString;
 
-            $numeroMinimoQuartos = 2;
-            $numeroMaximoQuartos = 3;
+            if (!empty($params)) {
+                logs()->info("Parâmetros da pesquisa", $params);
+                return Http::get($url);
+            }
 
-            $numeroMiniBanheiros = 2;
-            $numeroMaximoBaneiros = 3;
-
-            $valorMinimoCondominio = 100;
-            $valorMaximoCondominio = 500;
-
+            return Http::get("https://www.olx.com.br/imoveis/aluguel/estado-pa?bae=3&bas=2&gsp=1&q=venda&roe=3&ros=2&se=3&ss=2&cos=100&coe=500&rfs=100&rfs=111&rfs=115&rcf=203&rcf=207&rcf=205&rcf=202&rcf=200&rcf=209");
+        }
+        public static function params(): array
+        {
             //Detalhes do imóvel (rfs)
-            $academia       = 100;
-            $aquecimento    = 102;
+            $academia = 100;
+            $aquecimento = 102;
             $arCondicionado = 103;
-            $areaServico    = 104;
-            $internet       = 108;
+            $areaServico = 104;
+            $internet = 108;
+            $varanda = 115;
 
             //Detalhes do condomínio (rcf)
             $academiaCondominio = 200;
@@ -40,6 +43,30 @@
             $salaoFesta         = 208;
             $seguranca24h       = 209;
 
-            return Http::get("https://www.olx.com.br/imoveis/aluguel/estado-pa?bae=$numeroMaximoBaneiros&bas=$numeroMiniBanheiros&gsp=$vagasGaragem&q=venda&roe=$numeroMaximoQuartos&ros=$numeroMinimoQuartos&se=3&ss=2&cos=$valorMinimoCondominio&coe=$valorMaximoCondominio&rfs=100&rfs=111&rfs=115&rcf=203&rcf=207&rcf=205&rcf=202&rcf=200&rcf=209");
+            //Filters
+            $vagasGaragem = 1;
+            $numeroMinimoQuartos = 2;
+            $numeroMaximoQuartos = 3;
+
+            $numeroMiniBanheiros = 2;
+            $numeroMaximoBaneiros = 3;
+
+            $valorMinimoCondominio = 100;
+            $valorMaximoCondominio = 500;
+
+            return [
+                'bae' => $numeroMaximoBaneiros,
+                'bas' => $numeroMiniBanheiros,
+                'gsp' => $vagasGaragem,
+                'q' => 'venda',
+                'roe' => $numeroMaximoQuartos,
+                'ros' => $numeroMinimoQuartos,
+                'se' => 3,
+                'ss' => 2,
+                'cos' => 100,
+                'coe' => 500,
+                'rfs' => [$academia, 111, 115],
+                'rcf' => [$elevador, $portaria, $piscina, $condominioFechado, $academiaCondominio, $seguranca24h]
+            ];
         }
     }
